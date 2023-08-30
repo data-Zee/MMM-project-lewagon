e4b88ab997750b3e304568e2fd7a5216fe0358b3
Afsoon Ghazanfari <afsoon.ghazanfari@gmail.com>
2023-08-30 15:40:05 +0200

    fixing dataset

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
+import os
 
-
+#data = os.path.join(os.path.dirname(os.path.abspath(__file__)), "df.py"
 
 st.title("Marketing Mix Model")
 
 st.sidebar.markdown("# Dashboard")
+
+if __name__ == "__main__":
+    print(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "setup.py"))
