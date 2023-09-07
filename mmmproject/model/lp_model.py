839413a1dd762d8d8f56bc52a6a79500eb49cab2
noa ruccius <noa.fatal@gmail.com>
2023-09-07 16:28:42 +0200

    we are done!!!!

--- mmmproject/model/lp_model.py
+++ mmmproject/model/lp_model.py
@@ -13,55 +13,13 @@ import os
 import pickle
 from datetime import datetime
 from mmmproject.model.params import *
+from mmmproject.model.registry import minmax_fb, minmax_gg, minmax_tt
+
 
 
 '''creating a check if the date that was provided by the user is a holiday,
 a weekend, month of Jan or July and therefore creating new features'''
-
-# holiday feature
-class AddHolidaysTransformer(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
-        # This transformer doesn't need to learn any parameters during fitting,
-        # so we simply return self.
-        return self
-
-    def transform(self, X):
-        """Creates a new column with row value = 1 if the day is a Friday or Saturday and 0 if not."""
-        df = X.copy()  # Create a copy of the input DataFrame to avoid modifying it directly
-        de_holiday_list = []
-        for holiday in holidays.Germany(years=[2021,2022,2023]).items():
-            de_holiday_list.append(holiday)
-        de_holidays_df = pd.DataFrame(de_holiday_list, columns=["date", "holiday"])
-        de_holidays_df['date'] = pd.to_datetime(de_holidays_df['date'])
-        de_holidays_df.set_index('date', inplace=True)
-
-        at_holiday_list = []
-        for holiday in holidays.Austria(years=[2021,2022,2023]).items():
-            at_holiday_list.append(holiday)
-        at_holidays_df = pd.DataFrame(at_holiday_list, columns=["date", "holiday"])
-        at_holidays_df['date'] = pd.to_datetime(at_holidays_df['date'])
-        at_holidays_df.set_index('date', inplace=True)
-
-        # add DE holidays to df
-        merged_df = df.merge(de_holidays_df, how='left', left_index=True, right_index=True)
-        merged_df['de_holiday'] = merged_df.index.isin(de_holidays_df.index).astype(int)
-        merged_df.drop(columns=['holiday'], inplace=True)
-
-        # add AT holidays to df
-        at_holidays_df['at_holiday'] = 1 # add a 1 column to austrian holidays dataframe to help us merge with DE holidays
-        merged_df = merged_df.merge(at_holidays_df[['at_holiday']], how='left', left_index=True, right_index=True)
-        merged_df.head() # creates two columns (at_holiday_x, at_holiday_y), we only need one
-        merged_df['at_holiday'].fillna(0, inplace=True) # replace NaN (no holiday) with 0
-        merged_df['at_holiday'] = merged_df['at_holiday'].astype(int) # convert 1 and 0 to integers
-
-        # combine columns
-        merged_df['holiday'] = (merged_df['at_holiday'] | merged_df['de_holiday']).astype(int)
-        merged_df = merged_df.drop(columns = ['de_holiday', 'at_holiday']) # drop individual DE and AT rows
-
-        return merged_df[['holiday']]#.reset_index(drop=True)
-
-
-    # weekend feature
+# weekend feature
 
 class AddWeekendsTransformer(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
@@ -76,27 +34,6 @@ class AddWeekendsTransformer(BaseEstimator, TransformerMixin):
         df['fri_sat'] = ((weekday_values == 4) | (weekday_values == 5)).astype(int)
         return df[['fri_sat']]
 
-#month feature
-class AddMonthsTransformer(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
-        # This transformer doesn't need to learn any parameters during fitting,
-        # so we simply return self.
-        return self
-
-    def transform(self, X):
-        df = X.copy()  # Create a copy of the input DataFrame to avoid modifying it directly
-        df = (df
-        .pipe(add_time_features, month=True)
-        )
-
-        months_in_a_year = 12
-
-        df['sin_MonthYear'] = np.sin(2*np.pi*(df['month'])/months_in_a_year)
-        df['cos_MonthYear'] = np.cos(2*np.pi*(df['month'])/months_in_a_year)
-        df.drop(columns=['month'], inplace=True)
-
-        return df[['sin_MonthYear', 'cos_MonthYear']]
-
 
 def find_optimal_budget(TOTAL_DAILY_BUDGET, Date):
     df=pd.DataFrame([pd.to_datetime(Date)])
@@ -106,49 +43,33 @@ def find_optimal_budget(TOTAL_DAILY_BUDGET, Date):
 
     #treansforming the date df
 
-    time_features = ColumnTransformer(
-        [
-        ('holidays_pipe', Pipeline([
-                ('add_holidays', AddHolidaysTransformer())
-        ]), ['day']),
-        ('weekends_pipe', Pipeline([
-                ('add_weekends', AddWeekendsTransformer())
-        ]), ['day']),
-        ('months_pipe', Pipeline([
-                ('add_months', AddMonthsTransformer())
-        ]), ['day'])])
+    transformed_df = AddWeekendsTransformer().fit_transform(df)
+    transformed_df.columns = ['weekends']
 
-    transformed_df = time_features.fit_transform(df)
-    transformed_df = pd.DataFrame(transformed_df, columns = ['holiday', 'weekends', 'sin', 'cos'])
-    transformed_df
-
-    # input of total budget
-    TOTAL_DAILY_BUDGET= TOTAL_DAILY_BUDGET
 
     # model cvxpy
 
     # Variables
-    google   = cp.Variable(pos=True)
-    facebook = cp.Variable(pos=True)
-    tiktok  = cp.Variable(pos=True)
+    facebook_var = cp.Variable(pos=True)
+    google_var   = cp.Variable(pos=True)
+    tiktok_var  = cp.Variable(pos=True)
+
 
     # Constraint
-    constraint = [google + facebook + tiktok <= TOTAL_DAILY_BUDGET, google>=TOTAL_DAILY_BUDGET*0.04769543069031596, facebook>=TOTAL_DAILY_BUDGET*0.16612285992471154, tiktok>=TOTAL_DAILY_BUDGET*0.0361817093849725]
+    constraint = [google_var + facebook_var + tiktok_var <= TOTAL_DAILY_BUDGET, google_var>=TOTAL_DAILY_BUDGET*0.04769543069031596, facebook_var>=TOTAL_DAILY_BUDGET*0.16612285992471154, tiktok_var>=TOTAL_DAILY_BUDGET*0.0361817093849725]
 
-    betas=[-164.052961,-372.686127,265.076699,-23.341808,1.144179,5.504117,1.483814]
-    intercept=238.56530283612096
+
     # Objective
-    obj= cp.Maximize(intercept+betas[0] * transformed_df['holiday'][0] +betas[1] * transformed_df['weekends'][0] +betas[2] *transformed_df['sin'][0]+ betas[3] *transformed_df['cos'][0]+ betas[4] *(facebook)+betas[5] *(google)+betas[5] *(tiktok))
+    obj= cp.Maximize(intercept+ weekend* transformed_df['weekends'][0]+facebook *minmax_fb(facebook_var) + google *minmax_gg(google_var)+ tiktok*minmax_tt(tiktok_var))
 
     # Solve
     prob = cp.Problem(obj, constraint)
-    prob.solve(solver='ECOS', verbose=False)
-
+    prob.solve(solver='OSQP', verbose=False)
     # Print solution
     status=prob.status
-    value_Total_sales=prob.value
-    value_google_spend=google.value
-    value_facebook_spend=facebook.value
-    value_tiktok_spend=tiktok.value
+    value_Total_clicks=prob.value
+    value_google_spend=round(google_var.value)
+    value_facebook_spend=round(facebook_var.value)
+    value_tiktok_spend=round(tiktok_var.value)
 
-    return status, value_Total_sales, value_google_spend, value_facebook_spend, value_tiktok_spend
+    return status, value_Total_clicks, value_google_spend, value_facebook_spend, value_tiktok_spend
--- mmmproject/model/registry.py
+++ mmmproject/model/registry.py
@@ -1,9 +1,29 @@
 import pandas as pd
 import os
 import pickle
-from datetime import datetime
+import datetime
 from google.cloud import storage
 from mmmproject.model.params import *
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.preprocessing import MinMaxScaler
+#from mmmproject.model.weekendclass import AddWeekendsTransformer
+import traceback
+
+class AddWeekendsTransformer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        # This transformer doesn't need to learn any parameters during fitting,
+        # so we simply return self.
+        return self
+
+    def transform(self, X):
+        """Creates a new column with row value = 1 if the day is a Friday or Saturday and 0 if not."""
+        df = X.copy()  # Create a copy of the input DataFrame to avoid modifying it directly
+        weekday_values = df.index.weekday
+        df['fri_sat'] = ((weekday_values == 4) | (weekday_values == 5)).astype(int)
+        return df[['fri_sat']]
 
 def load_model(flag="default"):
     dirname = os.path.dirname(os.path.abspath(__file__))
@@ -35,7 +55,8 @@ def load_model(flag="default"):
             print(f"\n❌ No model found in GCS bucket {BUCKET_NAME}")
 
 def load_pipeline(flag="default"):
-    dirname = os.path.dirname(os.path.abspath(__file__))
+    # dirname = os.path.dirname(os.path.abspath(__file__))
+    dirname = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
     path_load = os.path.join(dirname,"..", "..", "pipelines")
 
     if MODEL_TARGET == "local":
@@ -49,32 +70,46 @@ def load_pipeline(flag="default"):
 
         try:
             # Create a blob object from the filepath
-            blob = bucket.blob("model.pkl")
+            blob = bucket.blob("pipelines/pipeline-2023-09-06 17:09:02.597279")
             print('blob_created')
             # Download the file to a destination
-            latest_model_path_to_save = os.path.join(MODEL_REGISTRY_PATH, 'pipeline.pkl')
-            blob.download_to_filename(latest_model_path_to_save)
+            # latest_model_path_to_save = os.path.join(MODEL_REGISTRY_PATH, 'pipeline.pkl')
+            local_path = os.path.join(dirname ,'pipelines','latest_pipeline.pkl')
+            (print(local_path))
+            blob.download_to_filename(local_path)
             print('blob_downloaded')
-            latest_model = pickle.load(open(latest_model_path_to_save, 'rb'))
+            latest_model = pickle.load(open(local_path, 'rb'))
+
 
             print("✅ Latest pipeline downloaded from cloud storage")
 
             return latest_model
-        except:
+        except Exception as e:
+            print(e)
             print(f"\n❌ No model found in GCS bucket {BUCKET_NAME}")
-
+            print(__name__)
 
     else:
         model = None
     return model
 
 if __name__ == "__main__":
-    storage_file_name = os.path.join(MODEL_REGISTRY_PATH, 'pipeline.pkl')
-    local_file_name = "/Users/noaruccius/code/data-Zee/MMM-project-lewagon/pipelines/pipeline-2023-09-06 17:09:02.597279.pkl"
+
+    local_file_name = "/Users/noaruccius/code/data-Zee/MMM-project-lewagon/pipelines/latest_pipeline.pkl"
     client = storage.Client()
     bucket = client.get_bucket(BUCKET_NAME)
-    blob = bucket.blob(storage_file_name)
+    blob = bucket.blob("pipelines/pipeline-2023-09-06 17:09:02.597279")
     blob.upload_from_filename(local_file_name)
+
+def minmax_fb(x):
+    scaled_fb = ((x - 0)/(2294.69 - 0))
+    return scaled_fb
+def minmax_gg(x):
+    scaled_gg = ((x - 0)/(480.66 - 0))
+    return scaled_gg
+def minmax_tt(x):
+    scaled_tt = ((x - 0)/(840.37- 0))
+    return scaled_tt
 
 
 # save as pickle file
@@ -83,6 +118,6 @@ if __name__ == "__main__":
 #     data_dir = 'raw_data'
 #     data_file = 'df_clean.csv'
 
-#     pipeline_save_path = os.path.join(parent_dir,'pipelines',f'pipeline-{datetime.now()}.pkl')
+#     pipeline_save_path = os.path.join(parent_dir,'pipelines',f'pipeline-{datetime.datetime.now()}.pkl')
 #     with open(pipeline_save_path, 'wb') as file:
 #        pickle.dump(model, file)
